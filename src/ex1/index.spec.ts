d726589290ed3c24c972dd3a3785211ad2f9a8e7
Arthur <arthur.wlodarczak@laposte.net>
2024-07-25 14:47:58 +0200

    feat : second test (exo1)

--- src/ex1/index.spec.ts
+++ src/ex1/index.spec.ts
@@ -1,12 +1,20 @@
-import { isRangeAvailable} from './index';
+import { isRangeAvailable } from './index';
 
 describe('isRangeAvailable', () => {
   it('devrait retourner vrai si la plage demandée est incluse dans la plage disponible', () => {
-      const debutDisponible = new Date('2023-01-01');
-      const finDisponible = new Date('2023-12-31');
-      const debutDemandee = new Date('2023-05-01');
-      const finDemandee = new Date('2023-05-31');
-      expect(isRangeAvailable(debutDemandee, finDemandee, debutDisponible, finDisponible)).toBe(true);
+    const debutDisponible = new Date('2023-01-01');
+    const finDisponible = new Date('2023-12-31');
+    const debutDemandee = new Date('2023-05-01');
+    const finDemandee = new Date('2023-05-31');
+    expect(isRangeAvailable(debutDemandee, finDemandee, debutDisponible, finDisponible)).toBe(true);
+  });
+
+  it('devrait retourner faux si la plage demandée commence avant la plage disponible', () => {
+    const debutDisponible = new Date('2023-01-01');
+    const finDisponible = new Date('2023-12-31');
+    const debutDemandee = new Date('2022-12-01');
+    const finDemandee = new Date('2023-05-31');
+    expect(isRangeAvailable(debutDemandee, finDemandee, debutDisponible, finDisponible)).toBe(false);
   });
 
 });
--- src/ex1/index.ts
+++ src/ex1/index.ts
@@ -3,7 +3,7 @@ export type DateRange = {
   endDate: Date;
 };
 
-export function isRangeAvailable(
+export function isRangeAvailable( // comme j'ai eu des problèmes d'imports de DateRange, j'ai décidé de remplacer les objets par des paramètres simples afin de continuer 
   requestedStart: Date, requestedEnd: Date, availableStart: Date, availableEnd: Date
 ): boolean {
   return requestedStart >= availableStart && requestedEnd <= availableEnd;
