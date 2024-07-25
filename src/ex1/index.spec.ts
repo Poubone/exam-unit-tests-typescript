55d0904974a2b8b4fc02b65d865f2752053b066f
Arthur <arthur.wlodarczak@laposte.net>
2024-07-25 14:49:43 +0200

    feat : quatrième test (exo1)

--- src/ex1/index.spec.ts
+++ src/ex1/index.spec.ts
@@ -25,6 +25,16 @@ describe('isRangeAvailable', () => {
     expect(isRangeAvailable(debutDemandee, finDemandee, debutDisponible, finDisponible)).toBe(false);
   });
 
+  it('devrait retourner faux si la plage demandée est complètement en dehors de la plage disponible', () => {
+    const debutDisponible = new Date('2023-01-01');
+    const finDisponible = new Date('2023-12-31');
+    const debutDemandee = new Date('2022-01-01');
+    const finDemandee = new Date('2022-12-31');
+    expect(isRangeAvailable(debutDemandee, finDemandee, debutDisponible, finDisponible)).toBe(false);
+  });
+
+
+
 });
 
 
