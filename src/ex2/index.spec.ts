49eabba4f3d19b9311647cb1dff2e74660d27261
Arthur <arthur.wlodarczak@laposte.net>
2024-07-25 15:00:36 +0200

    feat : premier test (exo2)

--- src/ex2/index.spec.ts
+++ src/ex2/index.spec.ts
@@ -1,3 +1,20 @@
-describe("submitOrder", () => {
-  // écrire les tests ici
+import { submitOrder, Order } from './index';
+import * as emailModule from './lib/email';
+
+jest.spyOn(emailModule, 'sendOrderEmail').mockImplementation(() => { });
+
+describe('Tests de la fonction submitOrder', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('devrait soumettre la commande et définir isSubmitted à true', () => {
+    const order: Order = { id: '123', isSubmitted: false };
+    const result = submitOrder(order);
+
+    expect(result.isSubmitted).toBe(true)
+    expect(emailModule.sendOrderEmail).toHaveBeenCalledTimes(1);
+    expect(emailModule.sendOrderEmail).toHaveBeenCalledWith('123');
+  });
+  
 });
